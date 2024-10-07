e72e688ca8d0cf7297c73c9cce50e65e5bb51cd9
PoseidonOcto <octopus0246@gmail.com>
2024-10-08 06:40:13 +1000

    Inlined variables, cleaning up code.

--- main.py
+++ main.py
@@ -258,31 +258,26 @@ def get_user_id(token: str) -> str:
 def add_fact():
     request_data = request.get_json()
     with rollback_on_err():
-        # TODO can we inline?
-        user_id = get_user_id(get_or_throw(request_data, 'oauth_token'))
-        claim_id = get_or_throw(request_data, 'claim_id'),
-        url = get_or_throw(request_data, 'url'),
-        triggering_text = get_or_throw(request_data, 'triggering_text'),
-        latest_date_triggered = get_or_throw(request_data, 'latest_date_triggered')
-
         new_data = Fact(
-            user_id=user_id,
-            claim_id=claim_id,
-            url=url,
-            triggering_text=triggering_text,
-            latest_date_triggered=latest_date_triggered,
+            user_id=get_user_id(get_or_throw(request_data, 'oauth_token')),
+            claim_id=get_or_throw(request_data, 'claim_id'),
+            url=get_or_throw(request_data, 'url'),
+            triggering_text=get_or_throw(request_data, 'triggering_text'),
+            latest_date_triggered=get_or_throw(request_data, 'latest_date_triggered'),
         )
 
+        # Get existing entry in database, if it exists.
         result = DB.session.execute(DB.select(Fact).where(
-            Fact.user_id == user_id and Fact.claim_id == claim_id and
-            Fact.url == url and Fact.triggering_text == triggering_text
+            Fact.user_id == new_data.user_id and Fact.claim_id == new_data.claim_id and
+            Fact.url == new_data.url and Fact.triggering_text == new_data.triggering_text
         )).one_or_none()
 
         if result is None:
             DB.session.add(new_data)
         else:
-            if result.Fact.latest_date_triggered < int(latest_date_triggered):
-                result.Fact.latest_date_triggered = latest_date_triggered
+            # Update entry to the one with the latest date.
+            if result.Fact.latest_date_triggered < int(new_data.latest_date_triggered):
+                result.Fact.latest_date_triggered = new_data.latest_date_triggered
 
     return None
 
